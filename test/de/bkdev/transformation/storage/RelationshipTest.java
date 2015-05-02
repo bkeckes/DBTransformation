ecdee3facc9ea403dd3c1ce5a763eb636bbe5e8f
Benni Keck <benjamin_keckes@web.de>
2015-05-02 15:25:25 +0200

    test fuer relationen

---
+++ test/de/bkdev/transformation/storage/RelationshipTest.java
@@ -0,0 +1,30 @@
+package de.bkdev.transformation.storage;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.bkdev.transformation.storage.Relationship;
+
+public class RelationshipTest {
+
+	@Test
+	public void testeRelationen(){
+		Node n = new Node("US");
+		n.addProperty("uid", "u01");
+		n.addProperty("name", "Date");
+		
+		Node b = new Node("BG");
+		b.addProperty("bid", "b02");
+		b.addProperty("title", "IT-Systems");
+		
+		Relationship r = new Relationship("isAdminFrom", new NodeTupel(n, b));
+		r.addProperty("since", "begin");
+//		System.out.println(r.toString());
+		
+		assertEquals("Date", r.getStart().getPropertyValue("name"));
+//		System.out.println(r.getStart());
+//		System.out.println(r.getEnd());
+		assertEquals("b02", r.getEnd().getPropertyValue("bid"));
+	}
+}
