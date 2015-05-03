6e6126d2fdbe918dd40ba69bb7961869f4e7e43b
Benni Keck <benjamin_keckes@web.de>
2015-05-03 15:11:35 +0200

    Tabelle und Property hinzugefuegt

--- src/de/bkdev/transformation/storage/relational/Property.java
+++ src/de/bkdev/transformation/storage/relational/Property.java
@@ -5,6 +5,44 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
 
 public class Property {
-	private Hashtable<String, String> propterty;
+	private boolean isPrimaryKey;
+	private boolean isForeignKey;
+	private String type;
+	private String name;
+	
+	public Property(boolean p, boolean f, String type, String name){
+		this.isPrimaryKey = p;
+		this.isForeignKey = f;
+		this.type = type;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isPrimaryKey() {
+		return isPrimaryKey;
+	}
+
+	public boolean isForeignKey() {
+		return isForeignKey;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public boolean isPropertyValid(){
+		//Wenn beide gesetzt sind ist es falsch
+		if(isPrimaryKey && isForeignKey)
+			return false;
+		if(type==null || type.isEmpty())
+			return false;
+		if(name==null || name.isEmpty())
+			return false;
+		
+		return true;
+	}
 	
 }
--- src/de/bkdev/transformation/storage/relational/Table.java
+++ src/de/bkdev/transformation/storage/relational/Table.java
@@ -1,10 +1,67 @@
 package de.bkdev.transformation.storage.relational;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
 public class Table {
 	private String name;
+	private Set<Property> property;
+	
 	public Table(String n){
 		this.name = n;
+		this.property = new HashSet<Property>();
 	}
+	
+	public void addProperty(Property prop){
+		if(prop!=null)
+			property.add(prop);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public Property getPrimaryKey(){
+		Iterator<Property> it = property.iterator();
+		
+		while(it.hasNext()){
+			Property p = it.next();
+			if(p.isPrimaryKey())
+				return p;
+		}
+		return null;
+	}
+	
+	public int getForeignKeyCount(){
+		Iterator<Property> it = property.iterator();
+		int count=0;
+		while(it.hasNext()){
+			Property p = it.next();
+			if(p.isForeignKey())
+				count++;
+		}
+		return count;
+	}
+	
+	public boolean isTableValid(){
+		//Ist Name gesetzt?
+		if(name!=null && !name.isEmpty()){
+			
+			//Sind alle Propertys in Ordnung?
+			for(Property p : property){
+				if(!p.isPropertyValid())
+					return false;
+			}
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	
 	
 	
 }
