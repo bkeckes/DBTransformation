e6e814e8b1bdf32f6b57128c13ea036076a6c92e
Benni Keck <benjamin_keckes@web.de>
2015-05-03 16:41:30 +0200

    Dumpreader Test angefangen

--- test/de/bkdev/transformation/parser/DumpReaderTest.java
+++ test/de/bkdev/transformation/parser/DumpReaderTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 
 public class DumpReaderTest {
 
-	
+	@Test
+	public void testeStringEinlesen(){
+		String temp = "DROP TABLE IF EXISTS `test`; CREATE TABLE `test` ( `id` int(11) NOT NULL auto_increment, `time` timestamp NOT NULL default CURRENT_TIMESTAMP,  PRIMARY KEY  (`id`)) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1; SET character_set_client = @saved_cs_client;";
+		
+	}
 
 }
